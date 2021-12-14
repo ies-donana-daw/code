13af8e3231759ead56d73288fbe0cdbefdfdea4f
DWECL <damian.leon@iesdonana.org>
2021-12-14 11:58:29 +0100

    ES Modules

---
+++ Extras/ESM/geometria.js
@@ -0,0 +1,16 @@
+export const PI_DOBLE = Math.PI*2;
+
+export function areaCirculo(radio){
+    return radio* radio * Math.PI;
+}
+
+export function perimetroCirculo(radio){
+    return radio* PI_DOBLE;
+}
+
+
+// export {
+//     PI_CUADRADO,
+//     areaCirculo,
+//     perimetroCirculo
+// }
---
+++ Extras/ESM/main.js
@@ -0,0 +1,29 @@
+// //Prueba1
+// import {PI_DOBLE, areaCirculo, perimetroCirculo} from "./geometria.js";
+
+// console.log(PI_DOBLE);
+// console.log(areaCirculo(2));
+// console.log(perimetroCirculo(2));
+
+
+// //Prueba2
+// import * as geom from "./geometria.js";
+
+// console.log(geom.PI_DOBLE);
+// console.log(geom.areaCirculo(2));
+// console.log(geom.perimetroCirculo(2));
+
+
+//Prueba3
+import {PI_DOBLE as PI2, areaCirculo as areaCirc, perimetroCirculo as perimCirc} from "./geometria.js";
+
+console.log(PI2);
+console.log(areaCirc(2));
+console.log(perimCirc(2));
+
+// //Prueba4
+// import {areaCirculo as areaCirc, perimetroCirculo as perimCirc} from "./geometria.js";
+
+// //console.log(PI2);
+// console.log(areaCirc(2));
+// console.log(perimCirc(2));
